9da42014ee6dbf46d0f4f0844452074710e6b13f
Luke Wang <lmwang@usc.edu>
2022-05-22 22:01:12 -0700

    Add files via upload

--- crudplus.cpp
+++ crudplus.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <string>
 using namespace std;
 
 
@@ -9,10 +10,11 @@ class Item
 		Item(int price_, string name_);
 		string name;
 		int price;
+		string deletionComment;
 };
 
 Item::Item(int price_, string name_):
-	name(name_), price(price_)
+	name(name_), price(price_), deletionComment("")
 {}
 
 void display(map<string, Item> inventory);
@@ -21,6 +23,7 @@ int main()
 {
 	char input = 'z';	
 	map<string, Item> inventory;
+	map<string, Item> recently_deleted;
 	string temp_name = "";
 	int temp_price;
 
@@ -31,6 +34,7 @@ int main()
 		cout << "(e) Edit an Item" << endl;
 		cout << "(d) Delete an Item" << endl;
 		cout << "(v) View inventory" << endl;
+		cout << "(r) View recently deleted" << endl;
 		cout << "(q) Quit" << endl;
 		
 		cin >> input;
@@ -39,12 +43,33 @@ int main()
 		{
 			cout << "What is the name of your item?" << endl;
 			cin >> temp_name;
-			cout << "What is the price?" << endl;
-			cin >> temp_price;
-			Item newItem(temp_price, temp_name);
-			inventory.insert ( make_pair(temp_name, newItem) );
+			char choice = 'o';
 
-			cout << endl;
+			if(inventory.find(temp_name) != inventory.end())
+			{
+				
+				while(choice != 'y' && choice != 'n')
+				{
+					cout << "Item exists, would you like to replace it? (y/n)" << endl;
+					cin >> choice;
+					if(choice == 'y')
+					{
+						cout << "What is the price?" << endl;
+						cin >> temp_price;
+						inventory.find(temp_name)->second.price = temp_price;
+						cout << endl;
+					}
+				}
+			}
+			else
+			{
+				cout << "What is the price?" << endl;
+				cin >> temp_price;
+				Item newItem(temp_price, temp_name);
+				inventory.insert ( make_pair(temp_name, newItem) );
+				cout << endl;
+			}
+			
 		}
 		else if (input == 'e')
 		{
@@ -74,6 +99,7 @@ int main()
 							cout << "New price: ";
 							cin >> temp_price;
 							inventory.find(temp_name)->second.price = temp_price;
+							change = 'e';
 						}
 						else if(change == 'n')
 						{
@@ -85,6 +111,7 @@ int main()
 							inventory.erase(temp_name);
 							Item newItem(temp_price, newName);
 							inventory.insert ( make_pair(newName, newItem) );
+							change = 'e';
 						}
 						else if(change == 'e')
 						{
@@ -116,8 +143,10 @@ int main()
 			}
 			else
 			{
+				
 				cout << "What item would you like to delete?" << endl;
 				display(inventory);
+				string comment;
 				cin >> temp_name;
 				if(inventory.find(temp_name) == inventory.end())
 				{
@@ -126,8 +155,15 @@ int main()
 					cout << endl;
 				}
 				else
-				{
+				{	
+					Item newItem(inventory.find(temp_name)->second.price, temp_name);
 					inventory.erase(temp_name);
+					cout << "Deletion comment: ";
+					cin>>ws;
+					getline(cin, comment);
+					newItem.deletionComment = comment;
+					recently_deleted.insert(make_pair(temp_name, newItem));
+					
 				}				
 			}	
 		}
@@ -135,6 +171,50 @@ int main()
 		{
 			display(inventory);
 		}	
+		else if(input == 'r')
+		{
+			cout << endl;
+
+			if(recently_deleted.empty())
+			{
+				cout << "Recently_deleted is empty" << endl;
+				cout << endl;
+			}
+			else
+			{
+				cout << "Recently deleted:" << endl;
+				for(map<string, Item>::const_iterator rit = recently_deleted.cbegin(); rit != recently_deleted.cend(); ++rit)
+				{
+				    cout << rit->first << ": " << rit->second.deletionComment << endl;
+				}			
+				cout << endl;	
+				cout << "Would you like to recover items? (y/n)" << endl;
+
+				char choice = 'o';
+				while (choice != 'n')
+				{
+					cin >> choice;
+					if(choice == 'y')
+					{
+						cout << "Which item?" << endl;
+						string recover;
+						cin >> recover;
+
+						if(recently_deleted.find(recover) != recently_deleted.end())
+						{
+							Item newItem(recently_deleted.find(recover)->second.price, recover);
+							inventory.insert ( make_pair(recover, newItem) );	
+							recently_deleted.erase(recover);					
+						}
+						else
+						{
+							cout << recover << " does not exist" << endl;
+						}
+						choice = 'n';
+					}			
+				}
+			}
+		}
 
 		else if(input == 'q')
 		{
